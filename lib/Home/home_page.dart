c3b48e70d44842a66e75971a8714f4531995fa33
karthik <karthikprabhu23223@gmail.com>
2023-12-11 14:52:23 +0530

    Page navigations

--- lib/Home/home_page.dart
+++ lib/Home/home_page.dart
@@ -32,85 +32,94 @@ class _MyHomePageState extends State<MyHomePage> {
         children: [
           Padding(
             padding: const EdgeInsetsDirectional.fromSTEB(16, 8, 0, 8),
-            child: Container(
-              width: 180,
-              height: 334,
-              decoration: BoxDecoration(
-                color: Colors.amberAccent,
-                boxShadow: const [
-                  BoxShadow(
-                    blurRadius: 4,
-                    color: Color(0x430F1113),
-                    offset: Offset(0, 1),
-                  )
-                ],
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: Padding(
-                padding: const EdgeInsetsDirectional.fromSTEB(8, 8, 8, 8),
-                child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Hero(
-                      tag: room['roomName'],
-                      transitionOnUserGestures: true,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(8),
-                        child: Image.network(
-                          'https://images.unsplash.com/photo-1601456713871-996c8765d82c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJlYWNoZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=900&q=60',
-                          width: double.infinity,
-                          height: 220,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding:
-                          const EdgeInsetsDirectional.fromSTEB(0, 16, 0, 0),
-                      child: Text(
-                        // 'Room Name',
-                        room['roomName'],
-                        style: const TextStyle(
-                          fontWeight: FontWeight.w500,
-                          fontSize: 20,
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsetsDirectional.fromSTEB(0, 4, 0, 0),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.max,
-                        children: [
-                          // ignore: prefer_const_constructors
-                          Text(
-                            '1.',
+            child: GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const MapLoc()),
+                );
+              },
+              child: Container(
+                width: 180,
+                height: 334,
+                decoration: BoxDecoration(
+                  color: Colors.amberAccent,
+                  boxShadow: const [
+                    BoxShadow(
+                      blurRadius: 4,
+                      color: Color(0x430F1113),
+                      offset: Offset(0, 1),
+                    )
+                  ],
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Padding(
+                  padding: const EdgeInsetsDirectional.fromSTEB(8, 8, 8, 8),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Hero(
+                        tag: room['roomName'],
+                        transitionOnUserGestures: true,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(8),
+                          child: Image.network(
+                            'https://images.unsplash.com/photo-1601456713871-996c8765d82c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJlYWNoZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=900&q=60',
+                            width: double.infinity,
+                            height: 220,
+                            fit: BoxFit.cover,
                           ),
-                          Text(
-                            // 'roomLocation',
-                            room['roomLocation'],
+                        ),
+                      ),
+                      Padding(
+                        padding:
+                            const EdgeInsetsDirectional.fromSTEB(0, 16, 0, 0),
+                        child: Text(
+                          // 'Room Name',
+                          room['roomName'],
+                          style: const TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding:
+                            const EdgeInsetsDirectional.fromSTEB(0, 4, 0, 0),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            // ignore: prefer_const_constructors
+                            Text(
+                              '1.',
+                            ),
+                            Text(
+                              // 'roomLocation',
+                              room['roomLocation'],
+                            ),
+                          ],
+                        ),
+                      ),
+                      const Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Padding(
+                            padding: EdgeInsetsDirectional.fromSTEB(4, 4, 0, 0),
+                            child: Text(
+                              'More info',
+                            ),
+                          ),
+                          Icon(
+                            Icons.navigate_next,
+                            color: Colors.black,
+                            size: 24,
                           ),
                         ],
                       ),
-                    ),
-                    const Row(
-                      mainAxisSize: MainAxisSize.max,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Padding(
-                          padding: EdgeInsetsDirectional.fromSTEB(4, 4, 0, 0),
-                          child: Text(
-                            'More info',
-                          ),
-                        ),
-                        Icon(
-                          Icons.navigate_next,
-                          color: Colors.black,
-                          size: 24,
-                        ),
-                      ],
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
@@ -333,6 +342,34 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                   ),
                 ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                        iconColor: Colors.blue,
+                        backgroundColor: Color.fromARGB(115, 108, 172, 90)
+                        // icon: const Icon(Icons.map_rounded),
+                        ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const MapLoc()),
+                      );
+                    },
+                    child: Text('Open Map'),
+                  ),
+                ),
+                // IconButton(
+                //   icon: const Icon(Icons.map),
+                //   iconSize: 40,
+                //   tooltip: 'Go to Map',
+                //   onPressed: () {
+                //     Navigator.push(
+                //         context,
+                //         MaterialPageRoute(builder: (context) => const MapLoc()),
+                //       );
+                //   },
+                // ),
               ],
             ),
           ),
