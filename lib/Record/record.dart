c367627c8790f426673c85b969d8a841dce65980
Karthik Prabhu <karthikprabhu23223@gmail.com>
2024-03-17 21:28:33 +0530

    Record Page created

--- lib/Record/record.dart
+++ lib/Record/record.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:map1/Home/profile_page.dart';
 
 class RecordLog extends StatefulWidget {
   const RecordLog({super.key});
@@ -10,6 +11,65 @@ class RecordLog extends StatefulWidget {
 class _RecordLogState extends State<RecordLog> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'TrackNow',
+            style: TextStyle(
+              fontSize: 31,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          centerTitle: true,
+          backgroundColor: const Color.fromARGB(0, 0, 0, 0),
+          actions: <Widget>[
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const ProfilePage(),
+                  ),
+                );
+              },
+              child: const Padding(
+                padding: EdgeInsets.only(right: 28.0),
+                child: CircleAvatar(
+                  radius: 23,
+                  backgroundColor: Colors.amber,
+                  child: CircleAvatar(
+                    radius: 20.0,
+                    backgroundImage: NetworkImage(
+                        'https://images.unsplash.com/photo-1590523741831-ab7e8b8f9c7f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YmVhY2hlc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60'),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+        body: Container(
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(16, 12, 0, 0),
+                  child: Text(
+                    'Record logs',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 25,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
