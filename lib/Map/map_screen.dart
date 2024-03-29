2b11ddb756ae521de5f50f1f39377e58cad37fc7
Karthik Prabhu <karthikprabhu23223@gmail.com>
2024-03-29 14:46:00 +0530

    icon button added on map_screen

--- lib/Map/map_screen.dart
+++ lib/Map/map_screen.dart
@@ -224,6 +224,8 @@ class MapScreenState extends State<MapScreen> {
                       borderRadius: BorderRadius.circular(10.0),
                       child: Image.network(
                         'https://images.unsplash.com/photo-1590523741831-ab7e8b8f9c7f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YmVhY2hlc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60',
+                        cacheWidth: 175,
+                        cacheHeight: 175,
                         fit: BoxFit.cover, // Adjust fit if needed
                       ),
                     ),
@@ -253,6 +255,33 @@ class MapScreenState extends State<MapScreen> {
                             fontSize: 12.0,
                             color: Colors.white70,
                           ),
+                        ),
+                        Row(
+                          children: [
+                            IconButton(
+                              color: const Color.fromARGB(255, 0, 0, 0),
+                              onPressed: () {
+                                zoomInMarker(element);
+                              },
+                              icon: const Row(
+                                children: [
+                                  Icon(
+                                    Icons.verified_rounded,
+                                    color: Colors.white,
+                                  ),
+                                  SizedBox(width: 2),
+                                  // Text(
+                                  //   'Reached',
+                                  //   style: TextStyle(
+                                  //     color: Colors.white,
+                                  //     fontSize:
+                                  //         12, // Adjust the font size as needed
+                                  //   ),
+                                  // ),
+                                ],
+                              ),
+                            )
+                          ],
                         ),
                       ],
                     ),
