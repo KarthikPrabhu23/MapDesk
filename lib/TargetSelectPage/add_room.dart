b4106aa7c246bb95c6787c43cfc62a49bcbbd8ea
karthik <karthikprabhu23223@gmail.com>
2024-04-05 22:34:28 +0530

    Revert "Created Widgets"
    This reverts commit 27ecb3ba9196d6f094da73c2fd787b4ed4f48df8.

--- lib/TargetSelectPage/add_room.dart
+++ lib/TargetSelectPage/add_room.dart
@@ -14,7 +14,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:map1/Map/classes.dart' as MyClass;
 import 'package:map1/Map/classes.dart';
 import 'package:map1/Record/components/task_complete_card.dart';
-import 'package:map1/TargetSelectPage/components/display_employee_assigned.dart';
 import 'package:map1/TargetSelectPage/components/map_dialog.dart';
 import 'package:map1/components/my_button.dart';
 
@@ -302,7 +301,7 @@ class _AddRoomState extends State<AddRoom> {
                             height: MediaQuery.of(context).size.height * 0.3,
                             // height: 600,
                             width: MediaQuery.of(context).size.width * 0.8,
-
+                            
                             decoration: const BoxDecoration(
                               gradient: LinearGradient(
                                 colors: [
@@ -345,7 +344,7 @@ class _AddRoomState extends State<AddRoom> {
                                               // if (user.assignedToEmployee) {
                                               //   return Text('username');
                                               // } else {
-                                              return Container();
+                                                return Container();
                                               // }
                                             },
                                           );
@@ -363,11 +362,17 @@ class _AddRoomState extends State<AddRoom> {
                 const SizedBox(
                   height: 15,
                 ),
-                DisplayEmployeeAssigned(
-                    IsEmployeeAssigned: IsEmployeeAssigned,
-                    selectedAssign: selectedAssign),
-                const SizedBox(
-                  height: 15,
+                Container(
+                  child: IsEmployeeAssigned
+                      ? Text(
+                          'Task Assigned to ${selectedAssign.username}',
+                          style: TextStyle(
+                            color: Colors.black87,
+                            fontSize: 19,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        )
+                      : SizedBox(),
                 ),
                 Center(
                   child: MyButton(
